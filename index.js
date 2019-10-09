86c0d6f9f52f2017b39b5e77bc2db522a7739f0f
Cronus <seer20582609@gmail.com>
2019-10-09 16:16:03 +0800

    web notify

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs");
 
 var server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end();
+    var path = url.parse(req.url).pathname;
+    console.log('Connect path: ' + path);
+    switch (path) {
+        case '/':
+            fs.readFile(__dirname + '/web/index.html', (err, data) => {
+                if (!err) {
+                    res.writeHead(200);
+                    res.write(data, 'utf-8');
+                    res.end();
+                } else {
+                    res.writeHead(404);
+                    res.write("404 Not found");
+                    res.end();
+                }
+            });
+    }
 });
 
 server.listen(80);
